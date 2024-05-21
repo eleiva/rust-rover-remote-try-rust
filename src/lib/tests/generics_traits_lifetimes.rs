0d246a5973d06b8d86d5c3b68ae53e5b682896af
Eduardo <eleiva@gmail.com>
2024-05-21 11:26:12 -0300

    upating with traits example

--- src/lib/tests/generics_traits_lifetimes.rs
+++ src/lib/tests/generics_traits_lifetimes.rs
@@ -27,20 +27,119 @@ fn find_largest_value_in_heap () {
     assert_eq!(largest, &100);
 }
 
-#[derive(Debug)]
-struct Fop<T> {
-    x: T
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
+
 #[test]
 fn test_generics() {
-    let num_1 = Fop {
-        x: 33
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+#[test]
+fn add_functionality_to_a_generic_type() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {} - p.y = {}", p.x(), p.y);
+}
+
+struct Point2<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point2<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point2<X2, Y2>) -> Point2<X1, Y2> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+#[test]
+fn test_generics_2() {
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+#[test]
+fn test_traits() {
+    let t1 = Tweet {
+        username: "edu".to_string(),
+        content : "my content".to_string(),
+        reply: false,
+        retweet: true
+    };
+    let na1 = NewsArticle {
+         headline: "headline of article".to_string(),
+         location: "chivilcoy".to_string(),
+         author: "maru".to_string(),
+         content: "soome content".to_string(),
+
     };
 
-    let num_2 = Fop {
-        x: 2.3
-    };
-
-    println!("{:?}", num_1);
-    println!("{:?}", num_2 );
+    assert_eq!("edu: my content", t1.summarize());
+    assert_eq!("headline of article, by maru (chivilcoy)", na1.summarize());
 }
